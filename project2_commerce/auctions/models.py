5dc4c3ca0351da89176fea109003967f394e6e8b
jefnilham <jefnilham1995@gmail.com>
2023-07-30 05:03:29 +0800

    get id as url next

--- project2_commerce/auctions/models.py
+++ project2_commerce/auctions/models.py
@@ -4,3 +4,22 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listings(models.Model):
+    listing_name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.listing_name
+
+class Biddings(models.Model):
+    listings = models.ForeignKey(Listings, on_delete=models.CASCADE)
+    bid_amount = models.IntegerField(max_length=200)
+    item_has_bid = models.BooleanField()
+    def __str__(self): 
+        return self.bid_amount
+    
+class Comments(models.Model):
+    listings = models.ForeignKey(Listings, on_delete=models.CASCADE)
+    comment_text = models.CharField(max_length=200)
+    item_has_comment = models.BooleanField()
+    def __str__(self):
+        return self.comment_text
