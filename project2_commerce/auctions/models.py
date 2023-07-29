ab3aa7348531c259ad31961f237b69a4859308e0
jefnilham <jefnilham1995@gmail.com>
2023-07-30 06:41:34 +0800

    stable version

--- project2_commerce/auctions/models.py
+++ project2_commerce/auctions/models.py
@@ -3,25 +3,34 @@ from django.db import models
 
 
 class User(AbstractUser):
-    any_listings_saved_to_watchlist = models.BooleanField()
+    #any_listings_saved_to_watchlist = models.BooleanField()
     pass
 
 class Listing(models.Model):
     listing_name = models.CharField(max_length=200)
     listing_description = models.CharField(max_length=1000)
-    listing_watchlisters = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing_price = models.IntegerField()
+    listing_category = models.CharField(max_length=200)
+    #listing_watchlisters = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.listing_name
 
 class Bidding(models.Model):
     listings = models.ForeignKey(Listing, on_delete=models.CASCADE)
-    bid_amount = models.IntegerField(max_length=200)
-    def __str__(self): 
-        return self.bid_amount, self.listings
+    bid_amount = models.IntegerField()
+    def __unicode__(self): 
+        return self.bid_amount
     
 class Comment(models.Model):
     listings = models.ForeignKey(Listing, on_delete=models.CASCADE)
-    comment_text = models.CharField(max_length=200)
+    new_comment_text = models.CharField(max_length=1000)
+    #display_comment = models.ManyToManyField(Listing)
     #item_has_comment = models.BooleanField()
     def __str__(self):
-        return self.comment_text, self.listings
+        return self.new_comment_text
+    
+class Watchlist(models.Model):
+    watchlist_listings = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    #watchlist_to_user = models.ManyToManyField(Listing)
+    #def __str__(self):
+        #return self.watchlist_listings
